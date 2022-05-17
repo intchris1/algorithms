7d7531759002192c5d6787aa4cba9a5037ebd830
Sbitneva Kristina <ksbitneva@siblion.ru>
2022-05-18 00:13:50 +0300

    dominator

---
+++ src/main/java/com/algorithms/chris/leader/Dominator.java
@@ -0,0 +1,46 @@
+package com.algorithms.chris.leader;
+
+/**
+ * Нужно вернуть индекс (любой) элемента, который повторяется больше N/2 раз.
+ * Алгоритм Бойера-Мура
+ * На вечеринке собрались N людей, и на каждом по одному элементу из массива.
+ * Когда встречаются двое, у которых элементы разные, они присаживаются это обсудить.
+ * В конце концов останутся стоять только люди с одинаковыми элементами; это может быть элемент, который встречался больше N/2 раз.
+ * <p>
+ * Return index (any) of an element if it occurs more than N/2 times.
+ * Boyer-Moore algorithm
+ * Suppose that you were to have a roomful of people each holding one element of the array.
+ * Whenever two people find each other where neither is holding the same array element as the other,
+ * the two of them sit down. Eventually, at the very end, if anyone is left standing, there's a chance that they're in the majority,
+ * and you can just check that element. As long as one element occurs with frequency at least N/2, you can guarantee that this approach will always find the majority element.
+ */
+public class Dominator {
+
+    public static int solution(int[] A) {
+        Integer candidate = null;
+        int confidence = 0;
+        for (int i : A) {
+            if (confidence == 0) {
+                candidate = i;
+                confidence++;
+            } else if (candidate == i) {
+                confidence++;
+            } else {
+                confidence--;
+            }
+        }
+        if (confidence > 0) {
+            confidence = 0;
+            for (int i = 0; i < A.length; i++) {
+                if (candidate == A[i]) {
+                    confidence++;
+                    if (confidence > A.length / 2) {
+                        return i;
+                    }
+                }
+            }
+
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/com/algorithms/chris/leader/LeaderTests.java
@@ -0,0 +1,30 @@
+package com.algorithms.chris.leader;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LeaderTests {
+
+    @ParameterizedTest
+    @MethodSource("dominator")
+    void dominator(int[] input, int expected) {
+        final int result = Dominator.solution(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> dominator() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 2, 3, 4, 3, 3, 3, -1}, 6),
+                Arguments.of(new int[]{3, 3, 3, 3, 3, 3, 3, 3}, 4),
+                Arguments.of(new int[]{1, 1, 1, 1, 2, 2, 2, 2}, -1),
+                Arguments.of(new int[]{2, 3, 2, 3, 2, 3, 2, 3, 2}, 8),
+                Arguments.of(new int[]{}, -1),
+                Arguments.of(new int[]{1}, 0)
+        );
+    }
+}
