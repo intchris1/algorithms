22b9ec8f036505a07f7bb18a93915d153b9a5228
Kristina Sbitneva <ksbitneva@siblion.ru>
2023-03-13 23:55:04 +0300

    IsValidPalindrome

---
+++ src/main/java/com/algorithms/chris/neetcode/two_pointers/ValidPalindrome.java
@@ -0,0 +1,30 @@
+package com.algorithms.chris.neetcode.two_pointers;
+
+/**
+ * Проверить, валиндный ли палиндром (исключая не буквы и не цифры, в нижнем регистре)
+ * Решение - использовать алгоритм two pointers.
+ *
+ * Check if palindrome is valid (comparing only alphanumeric characters lowercase)
+ * Solution - two pointers algorithm.
+ */
+public class ValidPalindrome {
+
+    public static boolean validPalindrome(String s) {
+        int leftIndex = 0;
+        int rightIndex = s.length() - 1;
+        while (leftIndex < rightIndex) {
+            while (leftIndex < rightIndex && !Character.isLetterOrDigit(s.charAt(leftIndex))) {
+                leftIndex++;
+            }
+            while (leftIndex < rightIndex && !Character.isLetterOrDigit(s.charAt(rightIndex))) {
+                rightIndex--;
+            }
+            if (leftIndex < rightIndex && Character.toLowerCase(s.charAt(leftIndex)) != Character.toLowerCase(s.charAt(rightIndex))) {
+                return false;
+            }
+            leftIndex++;
+            rightIndex--;
+        }
+        return true;
+    }
+}
